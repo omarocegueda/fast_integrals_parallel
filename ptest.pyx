fd053151ef8df8719cdfd5b5181d542d102c85b3
omarocegueda <jomarocegueda@gmail.com>
2016-06-03 16:41:13 -0700

    Use the same kind of temporary memory in both approaches

--- ptest.pyx
+++ ptest.pyx
@@ -13,71 +13,8 @@ def test_func():
     with nogil, parallel(num_threads=10):
         thread_id = threadid()
         printf("Thread ID: %d\n", thread_id)
-
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.cdivision(True)
-def rectangle_sums_sequential(double[:,:,:] I, int[:] m, double[:,:,:] out):
-    cdef:
-        cnp.npy_intp n0 = I.shape[0]
-        cnp.npy_intp n1 = I.shape[1]
-        cnp.npy_intp n2 = I.shape[2]
-        cnp.npy_intp m0 = m[0]
-        cnp.npy_intp m1 = m[1]
-        cnp.npy_intp m2 = m[2]
-        cnp.npy_intp i, j, k, prev_i, cur_i
-        double integral
-        double[:, :, :] T = np.zeros((2, n1, n2), dtype=np.float64)
-    with nogil:
-        cur_i = 1
-        for i in range(n0):
-            cur_i = 1 - cur_i
-            prev_i = 1 - cur_i
-            for j in range(n1):
-                for k in range(n2):
-                    # Start with last corner
-                    integral = I[i,j,k] # q=(0,0,0)
-                    # Add signed rectangles
-                    if i>0:
-                        integral += T[prev_i,j,k]# q=(1, 0, 0)
-                        if j>0:
-                            integral -= T[prev_i,j-1,k]# q=(1, 1, 0)
-                            if k>0:
-                                integral += T[prev_i,j-1,k-1]# q=(1, 1, 1)
-                        if k>0:
-                            integral -= T[prev_i,j,k-1]# q=(1, 0, 1)
-                    if j>0:
-                        integral += T[cur_i,j-1,k]# q=(0, 1, 0)
-                        if k>0:
-                            integral -= T[cur_i,j-1,k-1]# q=(0, 1, 1)
-                    if k>0:
-                        integral += T[cur_i,j,k-1]# q=(0, 0, 1)
-                        
-                    # Add displaced signed corners
-                    if i>=m0:
-                        integral -= I[i-m0,j,k]# q=(1, 0, 0)
-                        if j>=m1:
-                            integral += I[i-m0,j-m1,k]# q=(1, 1, 0)
-                            if k>=m2:
-                                integral -= I[i-m0,j-m1,k-m2]# q=(1, 1, 1)
-                        if k>=m2:
-                            integral += I[i-m0,j,k-m2]# q=(1, 0, 1)
-                    if j>=m1:
-                        integral -= I[i,j-m1,k]# q=(0, 1, 0)
-                        if k>=m2:
-                            integral += I[i,j-m1,k-m2]# q=(0, 1, 1)
-                    if k>=m2:
-                        integral -= I[i,j,k-m2]# q=(0, 0, 1)
-                        
-                    # Save current integral for future reference
-                    T[cur_i, j, k] = integral
-                    
-                    # Use integral of current rectangle
-                    out[i,j,k] = integral
-                    
-                    
-                    
+        
+        
 cdef double*** allocate_volume(int n0, int n1, int n2)nogil:
     cdef:
         double ***mem
@@ -102,6 +39,74 @@ cdef void free_volume(double ***mem, int n0, int n1)nogil:
             free(mem[i][j])
         free(mem[i])
     free(mem)
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+def rectangle_sums_sequential(double[:,:,:] I, int[:] m, double[:,:,:] out):
+    cdef:
+        cnp.npy_intp n0 = I.shape[0]
+        cnp.npy_intp n1 = I.shape[1]
+        cnp.npy_intp n2 = I.shape[2]
+        cnp.npy_intp m0 = m[0]
+        cnp.npy_intp m1 = m[1]
+        cnp.npy_intp m2 = m[2]
+        cnp.npy_intp i, j, k, prev_i, cur_i
+        double integral
+        double ***T
+    with nogil:
+        T = allocate_volume(2, n1, n2)
+        cur_i = 1
+        for i in range(n0):
+            cur_i = 1 - cur_i
+            prev_i = 1 - cur_i
+            for j in range(n1):
+                for k in range(n2):
+                    # Start with last corner
+                    integral = I[i,j,k] # q=(0,0,0)
+                    # Add signed rectangles
+                    if i>0:
+                        integral += T[prev_i][j][k]# q=(1, 0, 0)
+                        if j>0:
+                            integral -= T[prev_i][j-1][k]# q=(1, 1, 0)
+                            if k>0:
+                                integral += T[prev_i][j-1][k-1]# q=(1, 1, 1)
+                        if k>0:
+                            integral -= T[prev_i][j][k-1]# q=(1, 0, 1)
+                    if j>0:
+                        integral += T[cur_i][j-1][k]# q=(0, 1, 0)
+                        if k>0:
+                            integral -= T[cur_i][j-1][k-1]# q=(0, 1, 1)
+                    if k>0:
+                        integral += T[cur_i][j][k-1]# q=(0, 0, 1)
+                        
+                    # Add displaced signed corners
+                    if i>=m0:
+                        integral -= I[i-m0,j,k]# q=(1, 0, 0)
+                        if j>=m1:
+                            integral += I[i-m0,j-m1,k]# q=(1, 1, 0)
+                            if k>=m2:
+                                integral -= I[i-m0,j-m1,k-m2]# q=(1, 1, 1)
+                        if k>=m2:
+                            integral += I[i-m0,j,k-m2]# q=(1, 0, 1)
+                    if j>=m1:
+                        integral -= I[i,j-m1,k]# q=(0, 1, 0)
+                        if k>=m2:
+                            integral += I[i,j-m1,k-m2]# q=(0, 1, 1)
+                    if k>=m2:
+                        integral -= I[i,j,k-m2]# q=(0, 0, 1)
+                        
+                    # Save current integral for future reference
+                    T[cur_i][j][k] = integral
+                    
+                    # Use integral of current rectangle
+                    out[i,j,k] = integral
+        free_volume(T, 2, n1)  
+                    
+                    
+                    
+
     
                         
 @cython.boundscheck(False)
@@ -117,11 +122,13 @@ def rectangle_sums_parallel(double[:,:,:] I, int[:] m, double[:,:,:] out, int nu
         cnp.npy_intp m2 = m[2]
         cnp.npy_intp i, j, k, prev_i, cur_i, s_padded, s_start, s_end, s_per_thread, thread_id
         double*** T
+        double *integral
     set_num_threads(num_threads)
     with nogil, parallel():
         num_threads = openmp.omp_get_num_threads()
         thread_id = threadid()
         T = allocate_volume(2, n1, n2)
+        integral = <double*>malloc(sizeof(double))
         
         # Assign a consecutive range of slices to this thread
         s_per_thread = (n0 + num_threads - 1) / num_threads
@@ -141,43 +148,43 @@ def rectangle_sums_parallel(double[:,:,:] I, int[:] m, double[:,:,:] out, int nu
             for j in range(n1):
                 for k in range(n2):
                     # Start with last corner
-                    T[cur_i][j][k] = I[i,j,k] # q=(0,0,0)
+                    integral[0] = I[i,j,k] # q=(0,0,0)
                     # Add signed rectangles
                     if i>s_padded: # Our "zero" is now the first slice we are processing in this thread
-                        T[cur_i][j][k] += T[prev_i][j][k]# q=(1, 0, 0)
+                        integral[0] += T[prev_i][j][k]# q=(1, 0, 0)
                         if j>0:
-                            T[cur_i][j][k] -= T[prev_i][j-1][k]# q=(1, 1, 0)
+                            integral[0] -= T[prev_i][j-1][k]# q=(1, 1, 0)
                             if k>0:
-                                T[cur_i][j][k] += T[prev_i][j-1][k-1]# q=(1, 1, 1)
+                                integral[0] += T[prev_i][j-1][k-1]# q=(1, 1, 1)
                         if k>0:
-                            T[cur_i][j][k] -= T[prev_i][j][k-1]# q=(1, 0, 1)
+                            integral[0] -= T[prev_i][j][k-1]# q=(1, 0, 1)
                     if j>0:
-                        T[cur_i][j][k] += T[cur_i][j-1][k]# q=(0, 1, 0)
+                        integral[0] += T[cur_i][j-1][k]# q=(0, 1, 0)
                         if k>0:
-                            T[cur_i][j][k] -= T[cur_i][j-1][k-1]# q=(0, 1, 1)
+                            integral[0] -= T[cur_i][j-1][k-1]# q=(0, 1, 1)
                     if k>0:
-                        T[cur_i][j][k] += T[cur_i][j][k-1]# q=(0, 0, 1)
+                        integral[0] += T[cur_i][j][k-1]# q=(0, 0, 1)
                         
                     # Add displaced signed corners
                     if i>=s_padded + m0: # Our "zero" is now the first slice we are processing in this thread
-                        T[cur_i][j][k] -= I[i-m0,j,k]# q=(1, 0, 0)
+                        integral[0] -= I[i-m0,j,k]# q=(1, 0, 0)
                         if j>=m1:
-                            T[cur_i][j][k] += I[i-m0,j-m1,k]# q=(1, 1, 0)
+                            integral[0] += I[i-m0,j-m1,k]# q=(1, 1, 0)
                             if k>=m2:
-                                T[cur_i][j][k] -= I[i-m0,j-m1,k-m2]# q=(1, 1, 1)
+                                integral[0] -= I[i-m0,j-m1,k-m2]# q=(1, 1, 1)
                         if k>=m2:
-                            T[cur_i][j][k] += I[i-m0,j,k-m2]# q=(1, 0, 1)
+                            integral[0] += I[i-m0,j,k-m2]# q=(1, 0, 1)
                     if j>=m1:
-                        T[cur_i][j][k] -= I[i,j-m1,k]# q=(0, 1, 0)
+                        integral[0] -= I[i,j-m1,k]# q=(0, 1, 0)
                         if k>=m2:
-                            T[cur_i][j][k] += I[i,j-m1,k-m2]# q=(0, 1, 1)
+                            integral[0] += I[i,j-m1,k-m2]# q=(0, 1, 1)
                     if k>=m2:
-                        T[cur_i][j][k] -= I[i,j,k-m2]# q=(0, 0, 1)
+                        integral[0] -= I[i,j,k-m2]# q=(0, 0, 1)
                         
-                    
+                    T[cur_i][j][k] = integral[0]
                     # Use integral of current rectangle
                     if (s_start <= i) and (i < s_end):
-                        out[i,j,k] = T[cur_i][j][k]
+                        out[i,j,k] = integral[0]
         free_volume(T, 2, n1)                
                         
                        
